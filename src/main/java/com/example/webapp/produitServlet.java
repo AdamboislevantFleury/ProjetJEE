9f2eddfc9f31df2368bb2367238001db2fb1f239
alvisevenezia <wantoine@outlook.fr>
2023-11-13 08:57:03 +0100

    test bdd

--- src/main/java/com/example/webapp/produitServlet.java
+++ src/main/java/com/example/webapp/produitServlet.java
@@ -1,5 +1,6 @@
 package com.example.webapp;
 
+import jakarta.persistence.Persistence;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,9 +9,17 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @WebServlet(name = "produitServlet", value = "/produit")
 public class produitServlet extends HttpServlet {
+
+    private String url = "jdbc:postgresql://db.cvtaxdfnhdyuhqzaxrmk.supabase.co:5432/champions";
+    private String user = "postgres";
+    private String password = "U2xj7ROoI669A2bq";
+
 
     public produitServlet() {
         super();
@@ -19,6 +28,20 @@ public class produitServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("produitServlet doGet");
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection connection = DriverManager.getConnection(url, user, password);
+
+            System.out.println("Connected to PostgreSQL database!");
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
 
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("ashe", "ashe");

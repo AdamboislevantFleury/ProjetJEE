0a7d561612596e97383625a431c6e463f9d01276
marwane <marwane.laghzaoui@gmail.com>
2023-12-06 15:19:08 +0100

    communication entre le site et bdd pour gerer les droits

--- src/main/java/com/example/webapp/Rights.java
+++ src/main/java/com/example/webapp/Rights.java
@@ -1,29 +1,66 @@
 package com.example.webapp;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
-// Il me reste à faire en sorte de metre l'objet dans la bdd a coté du role user
 public class Rights {
-    private HashMap<String,Boolean> rights = new HashMap<>();;
+    // classe qui représente les droits, met à jours la bdd quand appellée et est "permanant" tant qu'on utilise le site (singleton)
+    private static Rights rights = null;;
+    private static HashMap<String,String> collection;
 
-    public Rights(int role){
-        if(role != 2){
-            rights.put("addChamp",true);  //ajouter un personnage
-            rights.put("deleteChamp",true);   //supprimer un personnage
-            rights.put("addUser",true);  //ajoouter un utilisateur depuis le site
-            rights.put("deleteUser",true);   //supprimer un utilsateur depuis la site
-            rights.put("changeId",true); //pouvoir modifier ou non ses identifiants
-        }
-        else{
-            rights.put("addChamp",false);  //ajouter un personnage
-            rights.put("deleteChamp",false);   //supprimer un personnage
-            rights.put("addUser",false);  //ajoouter un utilisateur depuis le site
-            rights.put("deleteUser",false);   //supprimer un utilsateur depuis la site
-            rights.put("changeId",true); //pouvoir modifier ou non ses identifiants
+    public Rights(String idUser) throws SQLException {
+        collection = new HashMap<>();
+        DatabaseUtils db = DatabaseUtils.getInstance();
+        String query = "SELECT * FROM "+db.getDatabase() +".permissions WHERE id_user = "+idUser;
+        ResultSet rs = null;
+        rs = db.sendQuery(query);
+        if(rs.next()){
+            collection.put("add",rs.getString("add"));  //ajouter un personnage
+            collection.put("delete",rs.getString("delete"));   //supprimer un personnage
+            collection.put("edit",rs.getString("edit"));  //modifier les droits d'un client
+            collection.put("promote",rs.getString("promote"));   //promouvoir client en admin
+            collection.put("id_user",rs.getString("id_user")); //identifiant du user
         }
     }
 
-    public HashMap<String,Boolean> getRights(){
-        return this.rights;
+    public void setRights(String right,String value) throws SQLException {
+        collection.put(right,value);
+        DatabaseUtils db = DatabaseUtils.getInstance();
+        String query = "UPDATE "+db.getDatabase() +".permissions SET "+right+" = "+value+" WHERE id_user = "+collection.get("id_user");
+        ResultSet rs = null;
+        rs = db.sendQuery(query);
+    }
+
+    public void setRights(HashMap<String,String> map) throws SQLException {
+        for(Map.Entry<String,String> mapEntry : map.entrySet()){
+            collection.put(mapEntry.getKey(),mapEntry.getValue());
+            DatabaseUtils db = DatabaseUtils.getInstance();
+            String query = "UPDATE "+db.getDatabase() +".permissions SET "+mapEntry.getKey()+" = "+mapEntry.getValue()+" WHERE id_user = "+collection.get("id_user");
+            ResultSet rs = null;
+            rs = db.sendQuery(query);
+        }
+    }
+
+    public HashMap<String, String> getCollection(){
+        return collection;
+    }
+
+    public static Rights getInstance(String idUser) throws SQLException {
+        if (rights == null) {
+            return new Rights(idUser);
+        }
+        System.out.println("Roles créés");
+        return rights;
+    }
+
+    public void affichage(){
+        System.out.println(collection.get("id_user"));
+        System.out.println(collection.get("add"));
+        System.out.println(collection.get("delete"));
+        System.out.println(collection.get("edit"));
+        System.out.println(collection.get("promote"));
     }
 }
